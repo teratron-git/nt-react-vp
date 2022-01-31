e40c4c8a770b2753d141a3da586936b3e42d88f5
Sergei Tereshkov <s.tereshkov@alarmtrade.ru>
2022-01-31 12:11:32 +0300

    initial project adjustment

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import { render } from "@testing-library/react"
+import { Provider } from "react-redux"
+import App from "./App"
+import { store } from "./store/store"
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />
     </Provider>
-  );
+  )
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
-});
+  // eslint-disable-next-line testing-library/prefer-screen-queries
+  expect(getByText(/learn/i)).toBeInTheDocument()
+})
