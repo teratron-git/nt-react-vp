0b824cfdb6245b68519d4a1757b4efa7ba095d33
Sergei Tereshkov <s.tereshkov@alarmtrade.ru>
2022-02-14 15:01:41 +0300

    add: CatalogPage loader

--- src/components/Main/CatalogPage/CatalogPage.tsx
+++ src/components/Main/CatalogPage/CatalogPage.tsx
@@ -9,9 +9,11 @@ import {
   getTopSalesAsync,
 } from "../../../store/mainSlice"
 import * as mainSelector from "../../../store/selectors"
-import { RootState } from "../../../store/store"
+import Preloader from "../Preloader"
 
 const CatalogPage = ({ form = false }) => {
+  const dispatch = useDispatch()
+
   const searchText = useSelector(mainSelector.getSearchText)
 
   const getCategoryValue = useSelector(mainSelector.getCategoryValue)
@@ -27,19 +29,21 @@ const CatalogPage = ({ form = false }) => {
   const [searchInput, setSearchInput] = useState(searchText)
   const [offset, setOffset] = useState(6)
 
-  const dispatch = useDispatch()
-
-  const [status, setStatus] = useState(getTopSalesStatus)
+  const [status, setStatus] = useState(getCatalogStatus)
   const [catalogData, setCatalogData] = useState(getCatalogValue)
   const [categoryData, setcategoryData] = useState(getCategoryValue)
 
-  // useEffect(() => {
-  //   setStatus(getTopSalesStatus)
-  // }, [getTopSalesStatus])
+  useEffect(() => {
+    setStatus(getCatalogStatus)
+  }, [getCatalogStatus])
 
-  // useEffect(() => {
-  //   setData(getTopSalesValue)
-  // }, [getTopSalesValue])
+  useEffect(() => {
+    setCatalogData(getCatalogValue)
+  }, [getCatalogValue])
+
+  useEffect(() => {
+    setcategoryData(getCategoryValue)
+  }, [getCategoryValue])
 
   const moreClickHandler = (e: any) => {
     e.preventDefault()
@@ -72,72 +76,81 @@ const CatalogPage = ({ form = false }) => {
   }
 
   return (
-    <section className="catalog">
-      <h2 className="text-center">Каталог</h2>
+    <>
+      {status === "loading" ? (
+        <section className="catalog">
+          <h2 className="text-center">Каталог</h2>
+          <Preloader />
+        </section>
+      ) : (
+        <section className="catalog">
+          <h2 className="text-center">Каталог</h2>
 
-      {form && (
-        <form
-          className="catalog-search-form form-inline"
-          onSubmit={(e) => {
-            e.preventDefault()
-            // setSearchInput
-            dispatch(changeSeachText(searchInput))
-          }}
-        >
-          <input
-            className="form-control"
-            placeholder="Поиск"
-            value={searchInput}
-            onChange={(e) => setSearchInput(e.target.value)}
-          />
-        </form>
-      )}
-
-      <ul className="catalog-categories nav justify-content-center">
-        {getCategoryValue.map((item) => (
-          <li className="nav-item" key={item.id}>
-            <Link
-              to=""
-              className={item.title === currentCategory.title ? "nav-link active" : "nav-link"}
-              onClick={categoryClickHandler}
+          {form && (
+            <form
+              className="catalog-search-form form-inline"
+              onSubmit={(e) => {
+                e.preventDefault()
+                // setSearchInput
+                dispatch(changeSeachText(searchInput))
+              }}
             >
-              {item.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <div className="row">
-        {getCatalogValue.map((item) => (
-          <div className="col-4" key={item.id}>
-            <div className="card catalog-item-card">
-              <img
-                src={item.images[0]}
-                className="card-img-top img-fluid"
-                alt={item.title}
-                onError={(e: React.SyntheticEvent) =>
-                  ((e.target as HTMLImageElement).src =
-                    "https://xn--80aafzai5cj.xn--p1ai/wp-content/uploads/2021/05/placeholder.jpg")
-                }
+              <input
+                className="form-control"
+                placeholder="Поиск"
+                value={searchInput}
+                onChange={(e) => setSearchInput(e.target.value)}
               />
-              <div className="card-body">
-                <p className="card-text">{item.title}</p>
-                <p className="card-text">{item.price}</p>
-                <NavLink to={`/products/${item.id}.html`} className="btn btn-outline-primary">
-                  Заказать
-                </NavLink>
+            </form>
+          )}
+
+          <ul className="catalog-categories nav justify-content-center">
+            {categoryData.map((item) => (
+              <li className="nav-item" key={item.id}>
+                <Link
+                  to=""
+                  className={item.title === currentCategory.title ? "nav-link active" : "nav-link"}
+                  onClick={categoryClickHandler}
+                >
+                  {item.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <div className="row">
+            {catalogData.map((item) => (
+              <div className="col-4" key={item.id}>
+                <div className="card catalog-item-card">
+                  <img
+                    src={item.images[0]}
+                    className="card-img-top img-fluid"
+                    alt={item.title}
+                    onError={(e: React.SyntheticEvent) =>
+                      ((e.target as HTMLImageElement).src =
+                        "https://xn--80aafzai5cj.xn--p1ai/wp-content/uploads/2021/05/placeholder.jpg")
+                    }
+                  />
+                  <div className="card-body">
+                    <p className="card-text">{item.title}</p>
+                    <p className="card-text">{item.price}</p>
+                    <NavLink to={`/products/${item.id}.html`} className="btn btn-outline-primary">
+                      Заказать
+                    </NavLink>
+                  </div>
+                </div>
               </div>
-            </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <div className="text-center">
-        {!getCatalogIsFinish ? (
-          <button type="button" className="btn btn-outline-primary" onClick={moreClickHandler}>
-            Загрузить ещё
-          </button>
-        ) : null}
-      </div>
-    </section>
+          <div className="text-center">
+            {!getCatalogIsFinish ? (
+              <button type="button" className="btn btn-outline-primary" onClick={moreClickHandler}>
+                Загрузить ещё
+              </button>
+            ) : null}
+          </div>
+        </section>
+      )}
+    </>
   )
 }
 
--- src/components/Main/TopSales/TopSales.tsx
+++ src/components/Main/TopSales/TopSales.tsx
@@ -20,34 +20,36 @@ const TopSales = () => {
     setData(getTopSalesValue)
   }, [getTopSalesValue])
 
-  return status === "loading" ? (
+  return (
     <>
-      <section className="top-sales">
-        <h2 className="text-center">Хиты продаж!</h2>
-      </section>
-      <Preloader />
-    </>
-  ) : data.length ? (
-    <section className="top-sales">
-      <h2 className="text-center">Хиты продаж!</h2>
-      <div className="row">
-        {data.map((item) => (
-          <div className="col-4" key={item.id}>
-            <div className="card">
-              <img src={item.images[0]} className="card-img-top img-fluid" alt={item.title} />
-              <div className="card-body">
-                <p className="card-text">{item.title}</p>
-                <p className="card-text">{item.price}</p>
-                <NavLink to={`/products/${item.id}.html`} className="btn btn-outline-primary">
-                  Заказать
-                </NavLink>
+      {status === "loading" ? (
+        <section className="top-sales">
+          <h2 className="text-center">Хиты продаж!</h2>
+          <Preloader />
+        </section>
+      ) : data.length ? (
+        <section className="top-sales">
+          <h2 className="text-center">Хиты продаж!</h2>
+          <div className="row">
+            {data.map((item) => (
+              <div className="col-4" key={item.id}>
+                <div className="card">
+                  <img src={item.images[0]} className="card-img-top img-fluid" alt={item.title} />
+                  <div className="card-body">
+                    <p className="card-text">{item.title}</p>
+                    <p className="card-text">{item.price}</p>
+                    <NavLink to={`/products/${item.id}.html`} className="btn btn-outline-primary">
+                      Заказать
+                    </NavLink>
+                  </div>
+                </div>
               </div>
-            </div>
+            ))}
           </div>
-        ))}
-      </div>
-    </section>
-  ) : null
+        </section>
+      ) : null}
+    </>
+  )
 }
 
 export default TopSales
